91f103c0398f4ea42efe75e15adfc0f510cbbc92
Romain Poyard <romainpoyard@gmail.com>
2025-11-10 14:49:15 +0100

    Initial Commit

---
+++ BuzzPlay/BuzzPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuzzPlayApp.swift
+//  BuzzPlay
+//
+//  Created by Apprenant 102 on 10/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct BuzzPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
