53fc4dd2f46072bf099583e22cb7d9404acfab3f
Seravea <romainpoyard@gmail.com>
2025-11-11 16:03:55 +0100

    update Architecture
    update Archi and add Core Models

---
+++ BuzzPlay/Core/Models/Team.swift
@@ -0,0 +1,19 @@
+//
+//  Team.swift
+//  BuzzPlay
+//
+//  Created by Apprenant 102 on 11/11/2025.
+//
+
+import Foundation
+import SwiftUI
+
+
+struct Team: Identifiable, Hashable, Codable, Equatable {
+    var id = UUID()
+    var name: String
+    var image: String?
+    var colorIndex: Int
+    var players: [Player] = []
+    var score: Int = 0
+}
