4daf37813496b04ef093a6aa844e5cad404d893a
Seravea <romainpoyard@gmail.com>
2025-11-11 16:11:10 +0100

    update Architecture

---
+++ BuzzPlay/Features/Buzzer/Views/BuzzerMasterView.swift
@@ -0,0 +1,18 @@
+//
+//  BuzzerMasterView.swift
+//  BuzzPlay
+//
+//  Created by Apprenant 102 on 11/11/2025.
+//
+
+import SwiftUI
+
+struct BuzzerMasterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BuzzerMasterView()
+}
