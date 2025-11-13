830326dcbb15acfb721aa605a357cdb19fc5f06f
Seravea <romainpoyard@gmail.com>
2025-11-13 10:29:52 +0100

    update
    new images and buzz animation

--- BuzzPlay/Features/TeamFeatures/Buzzer/Views/BuzzerPlayerView.swift
+++ BuzzPlay/Features/TeamFeatures/Buzzer/Views/BuzzerPlayerView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct BuzzerPlayerView: View {
+    @State var isTapped: Bool = false
     var body: some View {
         VStack {
             
+            ZStack {
+                
+                Image(.buttonFloor)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 300, height: 240)
+                Image(.buttonTap)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 320, height: 230)
+                    .padding(.bottom, isTapped ? 10 : 100)
+//
+            }
+            .onTapGesture {
+                isTapped.toggle()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.15) {
+                    isTapped.toggle()
+                }
+            }
+            .animation(.easeInOut(duration: 0.1), value: isTapped)
+            
+           
+//            .frame(height: 400)
         }
     }
 }
